23248cc9644d08404e960709ceb4d97268ba1ef1
Emeka Ugwuanyi <emylincon@gmail.com>
2019-05-11 15:47:54 +0100

    completed not tested
    Signed-off-by: Emeka Ugwuanyi <emylincon@gmail.com>

--- send_receive.py
+++ send_receive.py
@@ -1,6 +1,7 @@
 import socket
 import struct
-import sys
+import subprocess as sp
+from threading import Thread
 
 hosts = {}  # {hostname: ip}
 multicast_group = '224.3.29.71'
@@ -8,7 +9,7 @@ server_address = ('', 10000)
 
 # Create the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+sock.settimeout(0.2)
 # Bind to the server address
 sock.bind(server_address)
 # Tell the operating system to add the socket to the multicast group
@@ -16,12 +17,46 @@ sock.bind(server_address)
 group = socket.inet_aton(multicast_group)
 mreq = struct.pack('4sL', group, socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-# Receive/respond loop
 
-while True:
-    print('\nwaiting to receive message')
-    data, address = sock.recvfrom(1024)
 
-    print('received %s bytes from %s' % (len(data), address))
-    hosts[data.decode()] = address[0]
-    print(hosts)
+def send_message():
+    try:
+
+        # Send data to the multicast group
+        # print('sending "%s"' % message())
+        sent = sock.sendto(str.encode(message()), multicast_group)
+        print('\nmessage sent')
+
+    except Exception as e:
+        print(e)
+
+
+def message():
+    cmd = ['cat /etc/hostname']
+    hostname = str(sp.check_output(cmd, shell=True), 'utf-8')[0:-1]
+    return hostname
+
+
+def receive_message():
+    while True:
+        print('\nwaiting to receive message')
+        data, address = sock.recvfrom(1024)
+
+        print('received %s bytes from %s' % (len(data), address))
+        hosts[data.decode()] = address[0]
+        print(hosts)
+
+
+def main():
+    h1 = Thread(target=receive_message)
+    h1.start()
+    while True:
+        if input('Y/N').strip().lower() == 'y':
+            send_message()
+
+
+main()
+
+
+
+
